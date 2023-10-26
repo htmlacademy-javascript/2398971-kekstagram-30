e31f6a0c25e72527341e5706930ef1dffc2b0a96
MrScience969 <scieence969@gmail.com>
2023-10-26 20:08:11 +0700

    Функции возвращаются

--- js/functions.js
+++ js/functions.js
@@ -39,3 +39,12 @@ const checkMeetingTime = (timeStart, timeFinish, timeMeeting, meetingLength) => 
   }
 };
 
+// eslint-disable-next-line no-console
+console.log(getStringLength('проверяемая строка', 20)); // true
+// eslint-disable-next-line no-console
+console.log(checkPolydrome('топот')); // true
+// eslint-disable-next-line no-console
+console.log(getNumbersString('2023 год')); // 2023
+// eslint-disable-next-line no-console
+console.log(checkMeetingTime('08:00', '17:30', '14:00', 90)); // true
+
