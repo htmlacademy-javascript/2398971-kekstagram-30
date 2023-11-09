108bdc4b99fb90fa56f24cc64a2eb89fb03b922b
MrScience969 <scieence969@gmail.com>
2023-11-09 20:33:34 +0700

    Открывается и закрывается (часть 2)

--- js/img-comments.js
+++ js/img-comments.js
@@ -1,21 +1,24 @@
 const commentList = document.querySelector('.social__comments');
-const commentTemplate = document.querySelector('.social__comment');
+const commentTemplate = document.querySelector('#comment')
+  .content
+  .querySelector('.social__comment');
 const commentListFragment = document.createDocumentFragment();
 
-const createComments = (photos) => {
-  const comments = photos['comments'];
+const showComments = (photo) => {
+  const comments = photo.comments;
+  const newCommentElements = [];
   comments.forEach(({avatar, message, name}) => {
     const commentElement = commentTemplate.cloneNode(true);
+    commentElement.classList.add('hidden');
     commentElement.querySelector('.social__picture').src = avatar;
     commentElement.querySelector('.social__picture').alt = name;
     commentElement.querySelector('.social__text').textContent = message;
     commentListFragment.appendChild(commentElement);
+    newCommentElements.push(commentElement);
   });
-  while (commentList.firstChild) {
-    commentList.removeChild(commentList.firstChild);
-  }
 
-  return commentList.appendChild(commentListFragment);
+  commentList.replaceChildren(...newCommentElements);
 };
 
-export {createComments};
+export {showComments,commentList};
+
--- js/img-review.js
+++ js/img-review.js
@@ -1,66 +1,81 @@
 import {isEscapeKey} from './util.js';
-import {createComments} from './img-comments.js';
+import {showComments, commentList} from './img-comments.js';
 
-const reviewBigImg = (photos) => {
-  const usersImgs = document.querySelectorAll('.picture');
+const userPhoto = document.querySelector('.big-picture');
+const closeUserPhoto = userPhoto.querySelector('.big-picture__cancel');
 
-  usersImgs.forEach((userModalOpenImg) => {
-    const userModalImg = document.querySelector('.big-picture');
-    const userModalCloseImgs = userModalImg.querySelector('.big-picture__cancel');
+const commentsBotton = userPhoto.querySelector('.social__comments-loader');
+const COMMENT_STEP_ADD = 5;
+let visibleCommentCount = COMMENT_STEP_ADD;
 
-    const onDocumentKeydown = (evt) => {
-      if (isEscapeKey(evt)) {
-        evt.preventDefault();
-        closeUserModal();
-      }
-    };
+const onDocumentKeydown = (evt) => {
+  if (isEscapeKey(evt)) {
+    evt.preventDefault();
+    hideUserBigPhoto();
+  }
+};
 
-    function openUserModal () {
-      userModalImg.classList.remove('hidden');
-      document.querySelector('body').classList.add('modal-open');
-      document.addEventListener('keydown', onDocumentKeydown);
-    } // Открыть форму
+function hideUserBigPhoto () {
+  userPhoto.classList.add('hidden');
+  document.querySelector('body').classList.remove('modal-open');
+  document.removeEventListener('keydown', onDocumentKeydown);
+  visibleCommentCount = COMMENT_STEP_ADD;
+  userPhoto.querySelector('.comments-loader').classList.remove('hidden');
+} // Закрыть форму
 
-    function closeUserModal () {
-      userModalImg.classList.add('hidden');
-      document.querySelector('body').classList.remove('modal-open');
-      document.removeEventListener('keydown', onDocumentKeydown);
-    } // Закрыть форму
+function showUserBigPhoto () {
+  userPhoto.classList.remove('hidden');
+  document.querySelector('body').classList.add('modal-open');
+  document.addEventListener('keydown', onDocumentKeydown);
+} // Открыть форму
 
-    function getImgData() {
-      const idPhoto = userModalOpenImg.querySelector('img').id; // получить id фото в разметке
-      const dataPhoto = photos.find((item) => item.id === Number(idPhoto)); // найти элементт по id фото в массиве
+function getImgData(dataPhoto) {
+  const bigPhotoElement = userPhoto.querySelector('.big-picture__img');
+  const bigPhotoElementUrl = bigPhotoElement.querySelector('img');
+  const bigPhotoElementLikes = userPhoto.querySelector('.likes-count'); // Найти лайки в разметке
+  const bigPhotoElementCommentCountTotal = userPhoto.querySelector('.social__comment-total-count'); // Общее количество комментариев в разметке
+  const bigPhotoElementCommentDescription = userPhoto.querySelector('.social__caption'); // Найти описание фото в разметке
 
-      const bigPictureElementImg = userModalImg.querySelector('.big-picture__img');
-      const bigPictureElementImgUrl = bigPictureElementImg.querySelector('img');
-      const bigPictureElementLikes = userModalImg.querySelector('.likes-count'); // Найти лайки в разметке
-      const bigPictureElementCommentCountShown = userModalImg.querySelector('.social__comment-shown-count'); // Найти количество показанных комментариев в разметке
-      const bigPictureElementCommentCountTotal = userModalImg.querySelector('.social__comment-total-count'); // Общее количество комментариев в разметке
-      const bigPictureElementCommentDescription = userModalImg.querySelector('.social__caption'); // Найти описание фото в разметке
+  bigPhotoElementUrl.src = dataPhoto.url; // Ссылка на фото
+  bigPhotoElementLikes.textContent = dataPhoto.likes; //  Количество лайков
+  bigPhotoElementCommentCountTotal.textContent = dataPhoto.comments.length; // Общее количество комментариев
+  showComments(dataPhoto); // залить комментарии
+  bigPhotoElementCommentDescription.textContent = dataPhoto.description; // описание фото
+}
 
-      bigPictureElementImgUrl.src = dataPhoto.url; // Ссылка на фото
-      bigPictureElementLikes.textContent = dataPhoto.likes; //  Количество лайков
-      bigPictureElementCommentCountShown.textContent = dataPhoto.comments.length; // Количество показанных комментариев
-      bigPictureElementCommentCountTotal.textContent = dataPhoto.comments.length; // Общее количество комментариев
-      createComments(dataPhoto); // залить комментарии
-      bigPictureElementCommentDescription.textContent = dataPhoto.description; // описание фото
-    }
+const getVisibleComment = () => {
+  const allCommentsCount = commentList.children.length;
+  visibleCommentCount = Math.min(visibleCommentCount, allCommentsCount);
 
-    userModalOpenImg.addEventListener('click', () => {
-      openUserModal();
-      getImgData();
+  for (let i = 0; i < visibleCommentCount; i++) {
+    commentList.children[i].classList.remove('hidden');
+  }
+  userPhoto.querySelector('.social__comment-shown-count').textContent = visibleCommentCount;
 
-      userModalImg.querySelector('.social__comment-count').classList.add('hidden');
-      userModalImg.querySelector('.comments-loader').classList.add('hidden');
+  if (visibleCommentCount >= allCommentsCount) {
+    visibleCommentCount = allCommentsCount;
+    userPhoto.querySelector('.comments-loader').classList.add('hidden');
+  }
 
-    });
+  visibleCommentCount += COMMENT_STEP_ADD;
+};
 
-    userModalCloseImgs.addEventListener('click', () => {
-      closeUserModal();
-    });
+commentsBotton.addEventListener('click', () => {
+  getVisibleComment(commentList);
+});
 
+
+export const openBigPhoto = (photo) => {
+  showUserBigPhoto();
+  getImgData(photo);
+  getVisibleComment();
+};
+
+const initBigPhoto = () => {
+  closeUserPhoto.addEventListener('click', () => {
+    hideUserBigPhoto();
   });
 };
 
-export {reviewBigImg};
+export {initBigPhoto};
 
