32ef61b0ae07d2ed9d571a917891d9f82869f833
MrScience969 <scieence969@gmail.com>
2023-11-18 20:37:10 +0700

    Перламутровые пуговицы

--- js/main.js
+++ js/main.js
@@ -2,14 +2,16 @@
 import {createMiniatures} from './img-miniatures.js';
 import './img-review.js';
 import {hideUploadForm} from './img-upload.js';
-import {onFormSubmit} from './img-validate.js';
+import {addOnFormSubmit} from './img-validate.js';
 import {getData} from './api.js';
 import {showAlert} from './util.js';
+import {initImgFilters} from './img-filter.js';
 
 getData()
   .then((dataPhotos) => {
     createMiniatures(dataPhotos);
+    initImgFilters(dataPhotos);
   })
   .catch(() => showAlert());
 
-onFormSubmit(hideUploadForm);
+addOnFormSubmit(hideUploadForm);
--- js/util.js
+++ js/util.js
@@ -39,10 +39,31 @@ const showAlert = () => {
   }, ALERT_SHOW_TIME);
 };
 
+function debounce (callback, timeoutDelay = 500) {
+  let timeoutId;
+  return (...rest) => {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => callback.apply(this, rest), timeoutDelay);
+  };
+}
+
+function throttle (callback, delayBetweenFrames) {
+  let lastTime = 0;
+
+  return (...rest) => {
+    const now = new Date();
+    if (now - lastTime >= delayBetweenFrames) {
+      callback.apply(this, rest);
+      lastTime = now;
+    }
+  };
+}
 
 export {
   createRandomRangeGeneratorNoRepetitions,
   getRandomInteger,
   isEscapeKey,
-  showAlert
+  showAlert,
+  debounce,
+  throttle
 };
